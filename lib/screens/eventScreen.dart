f0d58cb80d7183b39e8ad5509d52f50ebb74033a
zuberypima <zuberypima12@gmail.com>
2024-06-05 14:24:37 +0300

    update done

--- lib/screens/eventScreen.dart
+++ lib/screens/eventScreen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:wegard/services/api_data.dart';
 
 class EventScreen extends StatefulWidget {
   const EventScreen({super.key});
+
+
+
 
   @override
   State<EventScreen> createState() => _EventScreenState();
 }
 
+
 class _EventScreenState extends State<EventScreen> {
+
+@override
+void initState()
+{
+super.initState();
+// fetchWeatherApi("52.52");
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Event Screen"),
+        child: InkWell(
+          onTap: () {
+            fetchWeatherApi();
+          },
+          child: Text("Event Screen")),
       ),
     );
   }
--- lib/services/api_data.dart
+++ lib/services/api_data.dart
@@ -1,7 +1,71 @@
-import 'package:http/http.dart' as http;
-class ApiDatas{
-   
-   dataCall (){
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
-   }
+// Future<void> fetchWeatherApi() async {
+
+//     final params = {
+//     "latitude": 52.52,
+//     "longitude": 13.41,
+//     "current": "temperature_2m",
+//   };
+//   final url = Uri.https('https://api.open-meteo.com', 'v1/forecast', params);
+//       // "https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=13.41&current=temperature_2m");
+
+//   try {
+//     final response = await http.get(url);
+//     if (response.statusCode == 200) {
+//       final decodedData = json.decode(response.body);
+//       // Process the weather data here
+//       print(decodedData);
+//       // ...
+//     } else {
+//       print("Error fetching weather data: ${response.statusCode}");
+//     }
+//   } catch (e) {
+//     print("Exception occurred: $e");
+//   }
+// }
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<void> fetchWeatherApi() async {
+  final params = {
+    "latitude": "52.52",
+    "longitude": "13.41",
+    // "current": "temperature_2m",
+    "current": [
+      "temperature_2m",
+      "relative_humidity_2m",
+      "apparent_temperature",
+      "is_day",
+      "precipitation",
+      "rain",
+      "showers",
+      "snowfall",
+      "weather_code",
+      "cloud_cover",
+      "pressure_msl",
+      "surface_pressure",
+      "wind_speed_10m",
+      "wind_direction_10m",
+      "wind_gusts_10m"
+    ],
+    "daily": ["temperature_2m_max", "temperature_2m_min", "sunrise", "sunset"]
+    // "hourly": "temperature_2m"
+  };
+  final url = Uri.https('api.open-meteo.com', '/v1/forecast', params);
+
+  try {
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final decodedData = json.decode(response.body);
+      // Process the weather data here
+      print(decodedData);
+      // ...
+    } else {
+      print("Error fetching weather data: ${response.statusCode}");
+    }
+  } catch (e) {
+    print("Exception occurred: $e");
+  }
 }
